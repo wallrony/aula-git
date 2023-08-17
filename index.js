0fdeab488e9a7a85020c2c5d1d0efd6c2b8c5fdf
Wallisson Rony <49597325+wallrony@users.noreply.github.com>
2023-08-16 21:58:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ function subtract(num1, num2) {
   console.log(num1 - num2);
 }
 
+function multiply(num1, num2) {
+  console.log(num1 * num2);
+}
+
 function soma(num1, num2) {
   console.log(num1 + num2);
 }
 
 subtract(1, 5);
 soma(1, 4);
+multiply(2, 5);
